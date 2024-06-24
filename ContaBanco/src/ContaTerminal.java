81a1faa89e473cf78fc4a4329211f3f0cac42c28
enoque <enoque.008@gmail.com>
2024-06-24 19:02:59 -0300

    conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe Scanner
+
+        Scanner teclado = new Scanner(System.in);
+
+        //Exibir as mensagens para o nosso usuário
+        //Obeter pela scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o Nome do Cliente !");
+        String nome = teclado.next();
+        
+        System.out.println("Por favor, digite a Agência !");
+        String agencia = teclado.next();
+
+        System.out.println("Por favor, digite a Conta !");
+        int conta = teclado.nextInt();
+        
+        System.out.println("Por favor, digite o Saldo !");
+        double saldo = teclado.nextDouble();           
+        
+        //Exibir a mensagem conta criada
+
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+
+
+
+
     }
 }
