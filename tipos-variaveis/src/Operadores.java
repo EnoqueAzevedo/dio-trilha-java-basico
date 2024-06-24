2643d3bb115d8351a5b79db54c0c5756d102bf2b
enoque <enoque.008@gmail.com>
2024-06-24 17:12:19 -0300

    commit2406

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,112 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+    /*
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+        int numero = 5;
+        numero = - numero;
+        numero = - numero;
+        int numero1 = 9;
+        numero1 = + numero1;
+        System.out.println(numero1);
+
+        System.out.println(numero);
+    
+
+        //classe Operadores.java
+        int numero = 5;
+		
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+    
+        
+        int a, b;
+
+        a = 5;
+        b = 6;
+        
+        String resultado = "";
+        // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+    
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(valor);
+        
+        System.out.println(resultado);
+        
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean sinNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual ao numeroDois? " + sinNao);
+
+        sinNao = numero1 != numero2;
+
+        System.out.println("numeroUm é Diferente do numeroDois? " + sinNao);
+
+        sinNao = numero1 < numero2;
+
+        System.out.println("numeroUm é Maior que numeroDois? " + sinNao);
+        */
+
+
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+            
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Umas das condições é verdadeira");
+        }
+        System.out.println("fim");
+    }
+
+
+    
+}
